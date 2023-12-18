88c1c4700ad91adb16d5e7e3b2d634faae8aab77
teosueur <97182374+teosueur@users.noreply.github.com>
2023-12-18 16:46:47 +0100

    carte zommable et dézoomable focntionnelle

--- application_suivi_des_travaux/lib/main.dart
+++ application_suivi_des_travaux/lib/main.dart
@@ -1,20 +1,20 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
-
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Angers Travaux Map',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -41,54 +41,80 @@ class _MyMapState extends State<MyMap> {
 
   Future<void> fetchData() async {
     final response = await http.get(
-        'https://api.url.de.la.ville.d.angers/info-travaux?fbclid=IwAR3pTTDTD7lqQICHFaCG9_WtI6P6vZc4vJzU-ydD5hkt2tgOTv3PhTc-Jho' as Uri);
+      'https://api.url.de.la.ville.d.angers/info-travaux?fbclid=IwAR3pTTDTD7lqQICHFaCG9_WtI6P6vZc4vJzU-ydD5hkt2tgOTv3PhTc-Jho' as Uri,
+    );
 
     if (response.statusCode == 200) {
-      // Parse JSON response
       Map<String, dynamic> data = json.decode(response.body);
-
-      // Assuming the API response has a 'works' field containing a list of works
       List<dynamic> works = data['works'];
 
-      // Add markers to the map
       for (var work in works) {
         double lat = work['latitude'];
         double lon = work['longitude'];
-        markers.add(Marker(
-          width: 40.0,
-          height: 40.0,
-          point: LatLng(lat, lon),
-          builder: (ctx) => Container(
-            child: Icon(Icons.work),
+        markers.add(
+          Marker(
+            width: 40.0,
+            height: 40.0,
+            point: LatLng(lat, lon),
+            builder: (ctx) => Container(
+              child: Icon(Icons.work),
+            ),
           ),
-        ));
+        );
       }
 
       setState(() {});
     } else {
-      // Handle error
       print('Failed to fetch data: ${response.statusCode}');
     }
+  }
+
+  void zoomIn() {
+    mapController.move(mapController.center, mapController.zoom + 1);
+  }
+
+  void zoomOut() {
+    mapController.move(mapController.center, mapController.zoom - 1);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Angers Travaux Map'),
-      ),
-      body: FlutterMap(
-        mapController: mapController,
-        options: MapOptions(
-          center: LatLng(47.47678, -0.56223),
-          zoom: 15.0,
-        ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: ['a', 'b', 'c'],
+      body: Stack(
+        children: [
+          FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+              center: LatLng(47.47678, -0.56223),
+              zoom: 15.0,
+            ),
+            layers: [
+              TileLayerOptions(
+                urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                subdomains: ['a', 'b', 'c'],
+              ),
+              MarkerLayerOptions(markers: markers),
+            ],
           ),
-          MarkerLayerOptions(markers: markers),
+          Positioned(
+            top: 100, // Ajuster la valeur pour déplacer les boutons plus bas
+            right: 16,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  onPressed: zoomIn,
+                  child: Icon(Icons.add),
+                  mini: true,
+                ),
+                SizedBox(height: 8),
+                FloatingActionButton(
+                  onPressed: zoomOut,
+                  child: Icon(Icons.remove),
+                  mini: true,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
