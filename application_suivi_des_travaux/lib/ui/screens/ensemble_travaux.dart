6d560f55332e7e5d2d7716d080158b698afef000
hugobls49 <hugoblois49@gmail.com>
2024-01-08 17:04:39 +0100

    MAJ UI

--- application_suivi_des_travaux/lib/ui/screens/ensemble_travaux.dart
+++ application_suivi_des_travaux/lib/ui/screens/ensemble_travaux.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import '../../blocs/travaux_cubit.dart';
 import '../../models/travaux.dart';
 
-class EnsembleTravaux extends StatelessWidget {
-  const EnsembleTravaux({Key? key});
+class EnsembleTravaux extends StatefulWidget {
+  const EnsembleTravaux({Key? key}) : super(key: key);
 
+  @override
+  _EnsembleTravauxState createState() => _EnsembleTravauxState();
+}
+
+class _EnsembleTravauxState extends State<EnsembleTravaux> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,21 +27,72 @@ class EnsembleTravaux extends StatelessWidget {
               itemCount: travauxList.length,
               itemBuilder: (context, index) {
                 final Travaux travaux = travauxList[index];
+
+                // Fonction pour formater la date
+                String formatDate(String dateString) {
+                  final DateTime dateTime = DateTime.parse(dateString);
+                  return DateFormat('dd/MM/yyyy').format(dateTime);
+                }
+
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('ID: ${travaux.id ?? ''}'),
-                    Text('Titre: ${travaux.titre ?? ''}'),
-                    Text('Description: ${travaux.description ?? ''}'),
-                    Text('Address: ${travaux.address ?? ''}'),
-                    Text('Start At: ${travaux.startAt ?? ''}'),
-                    Text('End At: ${travaux.endAt ?? ''}'),
-                    Text('Traffic : ${travaux.traffic ?? ''}'),
-                    Text('Contact: ${travaux.contact ?? ''}'),
-                    Text('Email: ${travaux.email ?? ''}'),
-                    Text('Tramway: ${travaux.isTramway ?? ''}'),
-                    Text('Longitude: ${travaux.long ?? ''}'),
-                    Text('Latitude: ${travaux.lat ?? ''}'),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Titre: ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: travaux.titre ?? ''),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Description : ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: travaux.description ?? ''),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Address : ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: travaux.address ?? ''),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Date de début : ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: formatDate(travaux.startAt ?? '')),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Date de fin : ',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(text: formatDate(travaux.endAt ?? '')),
+                        ],
+                      ),
+                    ),
+                    // Ajoutez les autres champs en utilisant le même modèle
                     const Divider(), // Ajout d'une ligne de division entre chaque travail
                   ],
                 );
