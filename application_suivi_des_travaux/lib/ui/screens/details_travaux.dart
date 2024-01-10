31c3729471e668146c13d779549fcf207046afc3
teosueur <97182374+teosueur@users.noreply.github.com>
2024-01-10 16:14:46 +0100

    Format de date modifié

--- application_suivi_des_travaux/lib/ui/screens/details_travaux.dart
+++ application_suivi_des_travaux/lib/ui/screens/details_travaux.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:application_suivi_des_travaux/blocs/travaux_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:application_suivi_des_travaux/models/travaux.dart';
+import 'package:intl/intl.dart';
 
 class DetailsTravaux extends StatelessWidget {
 
@@ -38,7 +39,7 @@ class DetailsTravaux extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const Text(
-              'Address:',
+              'Adresse:',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
@@ -52,9 +53,39 @@ class DetailsTravaux extends StatelessWidget {
               children: [
                 const Icon(Icons.access_time, size: 24),
                 const SizedBox(width: 8),
-                Text(
-                  'Date: ${travaux.startAt} - ${travaux.endAt}',
-                  style: const TextStyle(fontSize: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Débute le :',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      formatDate(travaux.startAt!) ?? 'Not specified',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(Icons.access_time, size: 24),
+                const SizedBox(width: 8),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Termine le:',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      formatDate(travaux.endAt!) ?? 'Not specified',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -113,4 +144,45 @@ class DetailsTravaux extends StatelessWidget {
     );
 
   }
+
+  String formatDate(String dateTimeString) {
+    DateTime dateTime = DateTime.parse(dateTimeString);
+    final day = dateTime.day.toString().padLeft(2, '0');
+    final month = dateTime.month.toString().padLeft(2, '0');
+    final year = dateTime.year.toString();
+
+    return '$day ${getMonthName(month)} $year';
+  }
+
+  String getMonthName(String month) {
+    switch (month) {
+      case '01':
+        return 'Janvier';
+      case '02':
+        return 'Février';
+      case '03':
+        return 'Mars';
+      case '04':
+        return 'Avril';
+      case '05':
+        return 'Mai';
+      case '06':
+        return 'Juin';
+      case '07':
+        return 'Juillet';
+      case '08':
+        return 'Août';
+      case '09':
+        return 'Septembre';
+      case '10':
+        return 'Octobre';
+      case '11':
+        return 'Novembre';
+      case '12':
+        return 'Décembre';
+      default:
+        return '';
+    }
+  }
+
 }
