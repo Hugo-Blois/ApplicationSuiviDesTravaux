87dc9ed8061e59fa14b51f1cca3d9d99e05d715d
hugobls49 <hugoblois49@gmail.com>
2024-01-13 15:10:44 +0100

    Ajout recherche de travaux par adresse

--- application_suivi_des_travaux/lib/ui/screens/ensemble_travaux.dart
+++ application_suivi_des_travaux/lib/ui/screens/ensemble_travaux.dart
@@ -1,9 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
-import '../../blocs/travaux_cubit.dart';
 import '../../models/travaux.dart';
+import '../../repositories/travaux_repository.dart';
 
 class EnsembleTravaux extends StatefulWidget {
   const EnsembleTravaux({Key? key}) : super(key: key);
@@ -13,22 +14,59 @@ class EnsembleTravaux extends StatefulWidget {
 }
 
 class _EnsembleTravauxState extends State<EnsembleTravaux> {
+  List<Travaux> _travaux = [];
+  List<Travaux> _filteredTravaux = [];
+  final TextEditingController _controller = TextEditingController();
+  Timer? _debounce;
+
+  @override
+  void initState() {
+    super.initState();
+    TravauxRepository.fetchAllTravaux().then((value) {
+      setState(() {
+        _travaux = value;
+        _filteredTravaux = _travaux;
+      });
+    });
+  }
+
+  void _onTravauxChanged(String value) {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        _filteredTravaux = _travaux
+            .where((travaux) =>
+                travaux.address?.toLowerCase().contains(value.toLowerCase()) ??
+                false)
+            .toList();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Détails des travaux :'),
       ),
-      body: BlocBuilder<TravauxCubit, List<Travaux>>(
-        builder: (context, travauxList) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
+      body: Column(
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  labelText: 'Rechercher des travaux par adresse',
+                ),
+                onChanged: _onTravauxChanged,
+              )),
+          Expanded(
             child: ListView.builder(
-              itemCount: travauxList.length,
+              itemCount: _filteredTravaux.length,
               itemBuilder: (context, index) {
-                final Travaux travaux = travauxList[index];
+                final Travaux travaux = _filteredTravaux[index];
 
-                // Fonction pour formater la date
                 String formatDate(String dateString) {
                   final DateTime dateTime = DateTime.parse(dateString);
                   return DateFormat('dd/MM/yyyy').format(dateTime);
@@ -37,69 +75,83 @@ class _EnsembleTravauxState extends State<EnsembleTravaux> {
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'Titre: ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: travaux.titre ?? ''),
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Titre: ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: travaux.titre ?? ''),
+                          ],
+                        ),
                       ),
                     ),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'Description : ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: travaux.description ?? ''),
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Description : ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: travaux.description ?? ''),
+                          ],
+                        ),
                       ),
                     ),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'Address : ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: travaux.address ?? ''),
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Address : ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: travaux.address ?? ''),
+                          ],
+                        ),
                       ),
                     ),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'Date de début : ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: formatDate(travaux.startAt ?? '')),
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Date de début : ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: formatDate(travaux.startAt ?? '')),
+                          ],
+                        ),
                       ),
                     ),
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'Date de fin : ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: formatDate(travaux.endAt ?? '')),
-                        ],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: 'Date de fin : ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(text: formatDate(travaux.endAt ?? '')),
+                          ],
+                        ),
                       ),
                     ),
-                    // Ajoutez les autres champs en utilisant le même modèle
-                    const Divider(), // Ajout d'une ligne de division entre chaque travail
+                    const Divider(),
                   ],
                 );
               },
             ),
-          );
-        },
+          )
+        ],
       ),
     );
   }
