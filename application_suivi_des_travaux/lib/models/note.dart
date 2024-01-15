509ee8ecc424008c818a8136da030147fe48ce21
hugobls49 <hugoblois49@gmail.com>
2024-01-15 17:22:00 +0100

    correction note

--- application_suivi_des_travaux/lib/models/note.dart
+++ application_suivi_des_travaux/lib/models/note.dart
@@ -1,21 +1,17 @@
-enum Etat { finished, unfinished }
-
 class Note {
   int? id;
   String? notes;
-  Etat etat;
 
-  Note(this.id, this.notes, this.etat);
+  Note(this.id, this.notes);
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'notes': notes,
-      'etat': etat,
     };
   }
 
   factory Note.fromJson(Map<String, dynamic> json) {
-    return Note(json['id'], json['notes'], json['etat']);
+    return Note(json['id'], json['notes']);
   }
 }
